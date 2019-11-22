1a261463f2af981c5556614367c44a8ddd65c016
Vladimir Chernov <dgudo73@mail.ru>
2019-11-22 23:50:47 +0300

    Добавил Vanta.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+VANTA.BIRDS({
+  el: "#intro",
+  backgroundColor: 0xfffcd7,
+  color1: 0xf7184f,
+  color2: 0xf48458,
+  colorMode: "lerp",
+  separation: 40.00,
+  alignment: 30.00,
+  cohesion: 21.00,
+  quantity: 4.00,
+  backgroundAlpha: 0.75
+})
